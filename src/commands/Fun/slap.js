f4099ca42a1055daaf126193f0d2f898132ce50a
ADG <2007andredoe@gmail.com>
2021-03-29 07:32:49 -0300

    amem?

--- src/commands/Fun/slap.js
+++ src/commands/Fun/slap.js
@@ -1,13 +1,9 @@
-const Discord = require('discord.js');
-const superagent = require('superagent');
-
-
 module.exports = class ExemploCommand {
     constructor(){
       return {
         permissoes: {
           membro: [], //Permissoes que o usuario necessita
-          bot: [], //Permissoes que o bot necessita
+          bot: ['EMBED_LINKS'], //Permissoes que o bot necessita
           dono: false //Se apenas nos devs podem usar o comando
         },
         pt: {
@@ -26,12 +22,15 @@ module.exports = class ExemploCommand {
     }
     
     async run(client, message, args, prefixo, idioma) {
+      const {MessageEmbed} = require('discord.js');
+      const superagent = require('superagent');
 
     if (!message.mentions.users.first()) return message.quote(`:x: ${message.author} **|** ${idioma.hug.user.replace('hug', 'slap').replace('abraçar', 'bater')}`);
 
     const { body } = await superagent
     .get("https://nekos.life/api/v2/img/slap");
-    const embed = new Discord.MessageEmbed()
+    
+    const embed = new MessageEmbed()
     .setColor("#ff09de")
     .setDescription(`**${message.author.username}** ${idioma.hug.acaba.replace('hugged', 'slapped').replace('abraçar', 'bater')} **${message.mentions.users.first().username}**`)
     .setImage(body.url) 
