658a6a6d9689283e2665b014c446e32a556ddbbb
ADG <2007andredoe@gmail.com>
2021-06-13 11:22:10 -0300

    teste

--- src/events/ready.js
+++ src/events/ready.js
@@ -9,13 +9,13 @@ module.exports = class ReadyEvent {
         require('colors');
         console.log(`[CLIENT] ${star.user.username+"#"+star.user.discriminator} Iniciada!`.dim.brightMagenta)
 
-        setTimeout(() => {
+        setInterval(() => {
             star.editStatus("online", {
                 game: star.user.username,
-                name: `s!help | ${star.guilds.size} guilds [v5.0.4]`,
+                name: `s!help | ${star.guilds.size} guilds [v5.0.6]`,
                 type: 5
             });
-        }, 1000 * 30)
+        }, 1000 * 100)
         star.music.init(star.user.id)
     }
 }
