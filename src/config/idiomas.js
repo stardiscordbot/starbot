8fe2197d456000910988ec8e2c38618ff2e149a1
ADG <2007andredoe@gmail.com>
2021-07-11 15:42:12 -0300

    nothing

--- src/config/idiomas.js
+++ src/config/idiomas.js
@@ -1,5 +1,5 @@
 module.exports = {
   pt_br: require('../idiomas/pt-br.json'),
   en_us: require('../idiomas/en-us.json'),
-  ja_jp: require('../idiomas/ja-jp.json')
+  pt_zeDroguinha: require('../idiomas/pt-zeDroguinha.json')
 }
