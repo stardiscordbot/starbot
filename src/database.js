a7e63d085e8e4f5ff100760cce89447b964afc7c
yADGithub <2007andredoe@gmail.com>
2021-08-04 17:13:09 -0300

    eta bixo

--- src/database.js
+++ src/database.js
@@ -1,4 +1,4 @@
-const DatabaseManager = require('denky-database')
+const DatabaseManager = require('./DenkyDatabase')
 
 // Essa será a varíavel que você usará para manipular a Database.
 global.db = new DatabaseManager('./data/base.json')
